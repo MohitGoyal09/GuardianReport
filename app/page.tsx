d1ad2ab3b7dfc220f30f905c1c3734a6cb72bf35
MohitGoyal09 <goel0632@gmail.com>
2024-12-18 17:50:15 +0530

    initialize Next.js project with TypeScript, Tailwind CSS, and ESLint configuration

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main className='relative px-6 pt-32'>
+      <div className = 'mx-auto max-w-5xl'>
+            {/* Hero */}
+            
+      </div>
+      </main>
+  );
+}
