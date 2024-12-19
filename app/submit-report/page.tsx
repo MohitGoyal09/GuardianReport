aac6ee6843fee68923c1cfc0f4840a7551f6f04f
MohitGoyal09 <goel0632@gmail.com>
2024-12-19 22:18:13 +0530

    add 'use client' directive to SubmitReport and LocationInput components

--- app/submit-report/page.tsx
+++ app/submit-report/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import  ReportWizard from '@/components/report/ReportWizard'
 export default function SubmitReport() {
--- components/report/LocationInput.tsx
+++ components/report/LocationInput.tsx
@@ -1,4 +1,5 @@
 // @ts-nocheck
+'use client'
 import { useState } from "react";
 import { AddressAutofill } from "@mapbox/search-js-react";
 
