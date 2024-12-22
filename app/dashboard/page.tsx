09887c20274d7807182b62990bc984d7ee6c2d2d
MohitGoyal09 <goel0632@gmail.com>
2024-12-23 02:05:13 +0530

    feat: initialize Dashboard component with session handling and imports

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { Report, ReportStatus, ReportType } from "@prisma/client";
+import { signOut } from "next-auth/react";
+
+export default function Dashboard(){
+      
+}
